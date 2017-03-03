6854f3e3e43e39243a70758ec141802e0282a86c
guitarfireyeah <matheson.lincoln12@hotmail.com>
2017-03-03 12:01:53 -0800

    updated ss methods

--- Spreadsheet/Spreadsheet/SpreadsheetMethods.cs
+++ Spreadsheet/Spreadsheet/SpreadsheetMethods.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,5 +10,9 @@ namespace Spreadsheet
 	interface SpreadsheetMethods 
 	{
 		object GetCellContents(string name);
+		void Save(TextWriter dest);
+		IEnumerable<String> GetNamesOfAllNonemptyCells();
+		object GetCellValue(String name);
+		ISet<String> SetContentsOfCell(String name, String content);
 	}
 }
