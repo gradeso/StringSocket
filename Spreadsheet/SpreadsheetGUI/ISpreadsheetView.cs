ff98ba3c7d616b06d3358b8838d5908645218514
WesleyBarth <Wes Barth@WESBARTH>
2017-03-07 13:02:37 -0700

    Fixed load to create new instance of a window

--- Spreadsheet/SpreadsheetGUI/ISpreadsheetView.cs
+++ Spreadsheet/SpreadsheetGUI/ISpreadsheetView.cs
@@ -14,7 +14,7 @@ namespace SpreadsheetGUI
 		string currentName { set; }
 		Dictionary<string, string> toUpdate { set; }
 
-		event Action<string, string> cellWithNameChagendContents;  //pass name and new contents
+		event Action<string, string> cellContentsChanged;  //pass name and new contents
 		event Action<string> cellHighlighted;               //pass name
 		event Action<string> loadSS;                        //pass filepath
         event Action<string> saveSS;
