c6154dbfbf565701ad9e863ffeeb1a8987f89761
WesleyBarth <wesbarth@gmail.com>
2017-03-08 14:01:23 -0700

    Added test project and stub

---
+++ Spreadsheet/ControllerTester/SpreadsheetWindowStub.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SpreadsheetGUI;
+
+namespace ControllerTester
+{
+    class SpreadsheetWindowStub : ISpreadsheetView
+    {
+        public string currentContents
+        {
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public string currentName
+        {
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public string currentValue
+        {
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public string message
+        {
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public Dictionary<string, string> toUpdate
+        {
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public event Action<string> cellHighlighted;
+        public event Action<string, string> cellWithNameChagendContents;
+        public event Action closeSS;
+        public event Action<string> loadSS;
+        public event Action<string> saveSS;
+
+        public void DoClose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Spreadsheet/SpreadsheetGUI/ISpreadsheetView.cs
+++ Spreadsheet/SpreadsheetGUI/ISpreadsheetView.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace SpreadsheetGUI
 {
-	interface ISpreadsheetView
+	public interface ISpreadsheetView
 	{
 		string message { set; }
 		string currentValue { set; }
